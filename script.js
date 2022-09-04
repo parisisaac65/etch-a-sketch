de7ae5cacc2767168affa3e87a078455cdab05e7
Paris Isaac <parisisaac65@gmail.com>
2022-09-04 16:28:59 -0400

    Create button to send the user a popup asking for the number of squares per size for the new grid
    Add feature to button that removes the existing grid before generating new grid

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 // Create a webpage with 16x16 grid of square divs 
-const container = document.querySelector('#container'); 
+let numOfSquares = 16;
+const container = document.querySelector('#container');
 
-function createGrid(){
-    for (let i = 1; i < 17; i++){
-        for(let i = 1; i < 17; i++){
+// Add a button asking for the number of squares per side for the new grid 
+const resizeBtn = document.querySelector('.resizeBtn'); 
+resizeBtn.addEventListener('click', resizeGrid); 
+
+function createGrid(numOfSquares){
+    container.style.cssText=`display:grid; width: 80%; grid-template-columns: repeat(${numOfSquares},1fr); background-color: rgba(149, 149, 149, 0.5)`;
+    for (let i = 1; i < numOfSquares + 1; i++){
+        for(let i = 1; i < numOfSquares + 1; i++){
             const cell = document.createElement('div');
-            cell.classList.add('cell'); 
+            cell.classList.toggle('cell'); 
             cell.textContent = i;
             container.appendChild(cell);
             cell.addEventListener('mouseover',() => {
                 displayHoverEffect(cell); 
-                // cell.style.cssText='background-color:black';
-            });
-        } 
+            }); 
+        }  
     }
 }
 
 function displayHoverEffect(cell){
     cell.style.cssText='background-color:black';
 }
+    
+createGrid(numOfSquares);
 
 
-createGrid();
+function resizeGrid(){
+    numOfSquares = prompt('Please enter the number of squares per side for new grid: ');
+    while (container.hasChildNodes()){
+        container.removeChild(container.firstChild);
+    }
+    createGrid(numOfSquares); 
+}
 
