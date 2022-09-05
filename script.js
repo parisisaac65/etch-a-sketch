4f52931da8f1c4ba3e8c56b2062447e5359150d4
Paris Isaac <parisisaac65@gmail.com>
2022-09-05 13:56:01 -0400

    Fix error that occurs when user clicks on resize button to create a new grid

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 // Create a webpage with 16x16 grid of square divs 
 let numOfSquares = 16;
+let totalGridSize = 0;
 const container = document.querySelector('#container');
 
 // Add a button asking for the number of squares per side for the new grid 
 const resizeBtn = document.querySelector('.resizeBtn'); 
 resizeBtn.addEventListener('click', resizeGrid); 
 
+
 function createGrid(numOfSquares){
-    container.style.cssText=`display:grid; width: 80%; grid-template-columns: repeat(${numOfSquares},1fr); background-color: rgba(149, 149, 149, 0.5)`;
-    for (let i = 1; i < numOfSquares + 1; i++){
-        for(let i = 1; i < numOfSquares + 1; i++){
-            const cell = document.createElement('div');
-            cell.classList.toggle('cell'); 
-            cell.textContent = i;
-            container.appendChild(cell);
-            cell.addEventListener('mouseover',() => {
-                displayHoverEffect(cell); 
-            }); 
-        }  
+    container.style.cssText=`display:grid; width: 80%; grid-template-columns: repeat(${numOfSquares},1fr);background-color: rgba(149, 149, 149, 0.5);`;
+    totalGridSize = numOfSquares ** 2; 
+    for (let i = 1; i <= totalGridSize; i++){
+        const cell = document.createElement('div');
+        cell.classList.toggle('cell'); 
+        cell.textContent = i;
+        container.appendChild(cell);
+        cell.addEventListener('mouseover',() => {
+            displayHoverEffect(cell); 
+        }); 
     }
 }
 
@@ -25,7 +26,7 @@ function displayHoverEffect(cell){
     cell.style.cssText='background-color:black';
 }
     
-createGrid(numOfSquares);
+createGrid(numOfSquares); 
 
 
 function resizeGrid(){
